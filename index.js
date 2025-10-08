3fffb0826608c5d1ad2585276a3232fb3cbf1b3d
KisKisBarsik <sadisintick@gmail.com>
2025-10-08 13:02:42 +0200

    Small changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.getElementById("submit0").addEventListener("click", function () {
   let country_code = document.getElementById("input1").value.toLowerCase()
   const myKey = "f90144eda55181ef3c4c1367f8e851ed"
 
+  // This API gives you a kelvin as a temperature, but to change it to celsius you have to write &units=metric after ${myKey}
   let myAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city_name},${country_code}&appid=${myKey}`
 
   let div = document.getElementById("mainContent")
@@ -16,17 +17,21 @@ document.getElementById("submit0").addEventListener("click", function () {
     .then(response => response.json())
     .then(data => {
 
-      console.log(data.name);
+      console.log(data);
           
       let p0 = document.getElementById("textInfo0")
       let p1 = document.getElementById("textInfo1")
       let p2 = document.getElementById("textInfo2")
       let p3 = document.getElementById("textInfo3")
 
-      p0.innerText = `${data.name}`
-      p1.innerText = ``
-      p2.innerText = ``
-      p3.innerText = ``
+      let celsius = Math.round((data.main.temp - 273.15),2)
+      let maxTemp = Math.round(data.main.temp_max - 273.15)
+      let minTemp = Math.round(data.main.temp_min - 273.15)
+
+      p0.innerText = `${data.name}, ${data.sys.country}`
+      p1.innerText = `${celsius} ° C`
+      p2.innerText = `${data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)}`
+      p3.innerText = `${minTemp} ° C / ${maxTemp} ° C`
             
     })
 
